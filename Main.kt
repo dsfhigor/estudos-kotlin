3fc05d875df136b48c4b948a81550be90c4a4980
Hígor da Silva Flores <dsfhigor@gmail.com>
2023-09-13 23:44:48 -0300

    primeiros passos kotlin

---
+++ Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Higor")
+
+    var num1 = 10
+    var num2 = 99
+    var soma = num1 + num2
+
+    println(soma++)
+    println(soma)
+
+    fun cidade(cidade: String): String {
+        return cidade
+    }
+    println(cidade("Santa Maria"))
+}
